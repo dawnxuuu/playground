0e776bcc218e2a883cbce62fcfa163fc5c5402be
徐晓 <xu.xiao@zhaopin.com.cn>
2021-02-15 19:06:49 +0800

    delete some

--- src/new-job-page/3-city/get-around-cities-detail.js
+++
@@ -1,7 +0,0 @@
-const aroundCitiesDetail = require('./around-cities-detail')
-
-function getAroundCitiesDetail(cityId) {
-  return ['周边城市', aroundCitiesDetail[cityId]]
-}
-
-console.log(getAroundCitiesDetail('530'))
